76dfb962aa300aeb06d70b381b673c4dbeae274b
Jordan Demmouche <jordand75020@hotmail.fr>
2017-09-06 19:33:49 +0200

    fix form active admin

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -2,8 +2,6 @@ ActiveAdmin.register Review do
 permit_params :content, :user_id, :guide_id, :checked
       form do |f|
     f.inputs "Review" do
-      f.input :user_id
-      f.input :guide_id
       f.input :content
       f.input :checked
     end
