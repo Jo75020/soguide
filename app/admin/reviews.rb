823e2fbda08d7d431c97c6020b34d52adf1136cc
Jordan Demmouche <jordand75020@hotmail.fr>
2017-09-06 19:59:46 +0200

    reviews checkbox

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -3,7 +3,7 @@ permit_params :content, :user_id, :guide_id, :checked
       form do |f|
     f.inputs "Review" do
       f.input :content
-      f.input :checked
+      f.input :checked, checkbox: false
     end
     f.actions
   end
