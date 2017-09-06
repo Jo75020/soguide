1df9a7d8902055ca59444d5a86a0210757fd3acf
Jordan Demmouche <jordand75020@hotmail.fr>
2017-09-06 20:27:03 +0200

    fix some stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :a_savoir]
 
   def home
+    unless params[:search] == ""
+      @results = Country.search_name(params[:search])
+    end
+    if params[:search] == "" || params[:search].nil?
+      @results = Country.where.not(latitude: nil, longitude: nil)
+    end
   end
 
   def index
