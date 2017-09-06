d69e12bff61d0836c8dcdc01645ac0dc361e8496
Jordan Demmouche <jordand75020@hotmail.fr>
2017-09-06 19:32:11 +0200

    add reviews to active_admin

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Review do
 permit_params :content, :user_id, :guide_id, :checked
+      form do |f|
+    f.inputs "Review" do
+      f.input :user_id
+      f.input :guide_id
+      f.input :content
+      f.input :checked
+    end
+    f.actions
+  end
 end
