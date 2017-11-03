9ec5ea2d82b5fb963de541d69cbacec5a201fe1a
Jordan Demmouche <jordand75020@hotmail.fr>
2017-11-03 13:59:05 +0100

    fix again and again

--- db/migrate/20171103125054_remove_attachinary.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAttachinary < ActiveRecord::Migration[5.0]
-def up
-  drop_table :attachinary_files
-end
-end
---
+++ db/migrate/20171103125353_attachinary_migration.rb
@@ -0,0 +1,17 @@
+class AttachinaryMigration < ActiveRecord::Migration[5.0]
+  def change
+    create_table "attachinary_files", force: :cascade do |t|
+    t.integer  "attachinariable_id"
+    t.string   "attachinariable_type"
+    t.string   "scope"
+    t.string   "public_id"
+    t.string   "version"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "format"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+  end
+end
