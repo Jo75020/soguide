7263ed5a80c2226a420e918b1a888487945e5d09
Jordan Demmouche <jordand75020@hotmail.fr>
2017-11-03 13:28:57 +0100

    migration error

--- db/migrate/20171028141246_remove_images_from_country.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImagesFromCountry < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :countries, :images, :string, array: true
-  end
-end
--- db/migrate/20171028141332_add_images_as_json_to_country.rb
+++
@@ -1,5 +0,0 @@
-class AddImagesAsJsonToCountry < ActiveRecord::Migration[5.0]
-  def change
-        add_column :countries, :images, :json, default: []
-  end
-end
