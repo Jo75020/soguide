13c015f8a6e6bdfa483e03b98bba6609125ae9c4
Jordan Demmouche <jordand75020@hotmail.fr>
2017-11-03 13:17:27 +0100

    migration error

--- db/migrate/20171103113708_remove_column_from_coutries.rb
+++ db/migrate/20171103113708_remove_column_from_coutries.rb
@@ -1,6 +1,5 @@
 class RemoveColumnFromCoutries < ActiveRecord::Migration[5.0]
   def change
-    remove_column :countries, :photos, :string
     remove_column :countries, :images, :string
   end
 end
