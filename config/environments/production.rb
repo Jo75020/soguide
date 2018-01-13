881b796c7ae4114ff3cf468fdd2d87d5fca9a3b6
Jordan Demmouche <jordand75020@hotmail.fr>
2018-01-13 20:52:17 +0100

    add gmail for sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://soguide.herokuapp.com" }
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "http://soguide.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://soguide.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
