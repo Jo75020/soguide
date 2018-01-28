c8836fe6eea569d340d4689876f19fe9665b54d3
Jordan Demmouche <jordand75020@hotmail.fr>
2018-01-28 21:07:10 +0100

    uploader reviews and export csv

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -11,5 +11,17 @@ permit_params :content, :user_id, :guide_id, :checked
   end
 
 
+  csv col_sep: ';', force_quotes: true do
+    column('ID') { |r| r.id }
+    column('User_email') { |r| r.user.email }
+    column('User_first_name') { |r| r.user.first_name}
+    column('User_last_name') { |r| r.user.last_name}
+    column('User_password') { |r| r.user.password}
+    column('Date') { |r| r.fake_date}
+    column('Body review') { |r| r.content}
+    column('Guide Csv') { |r| r.guide.user.email}
+  end
+
+
 
 end
